27bd06a12be507591b97fc3bab609bab4f5e7ff9
“devwonny” <devwonny@gmail.com>
2024-08-10 21:41:51 +0900

    Initial commit

---
+++ src/main/java/com/sparta/msa_exam/product/Product.java
@@ -0,0 +1,14 @@
+package com.sparta.msa_exam.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	private Long productId;
+	private String name;
+	private Integer supplyPrice;
+}
